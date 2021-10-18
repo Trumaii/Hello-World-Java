d1d9e5d8a45fba2f539b47c77bb0d53235363223
Trumai Vilhena <trumaiv@gmail.com>
2021-10-17 22:01:03 -0300

    variaveis

---
+++ exerciciosvariaveis/src/exerciciosVariaveisint/VariaveisExercicios.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package exerciciosVariaveisint;
+
+/**
+ * @author truma
+ * exercicios de variaveis inteiro double e string
+ */
+public class VariaveisExercicios {
+
+	
+	public static void main(String[] args) {
+		
+		int a = 33;
+		int b = 22;
+		     
+		 System.out.println(a + b);
+		 System.out.println( "A soma esta correta");
+		      
+		 double c = 1.33;
+		 double d = 2.22;
+		 System.out.println( d - c );
+		    
+		 String frase ; 
+		 frase = "Isso é tudo pessoal!";
+		 System.out.println(frase);
+ 
+	}
+
+}
